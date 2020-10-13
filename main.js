ae5a2be6b2788e9a82a74ea3e691898cfc99426a
Spirit412 <spirit412@gmail.com>
2020-10-13 17:11:13 +0300

    ДЗ-9 Функции перенесены в методы класса

--- main.js
+++ main.js
@@ -1,78 +1,130 @@
-import Pokemon from "./pokemon.js";
 import {random, countBtn, generateLog, outputLog} from "./utils.js";
-import {pokemons} from "./pokemons.js";
-// import {renderHP, renderHPLife, renderProgressHP} from "./render.js"
-//получаем индекс первого персонажа
-const rnd1 = random([0, pokemons.length -1])
-//получаем индекс второго персонажа
-const rnd2 = random([0, pokemons.length -1 ])
+import Pokemon from "./pokemon.js";
 
-const play1 = pokemons.find(item => item.name === pokemons[rnd1].name);
-console.log(play1);
+class Game {
 
-const play2 = pokemons.find(item => item.name === pokemons[rnd2].name);
-const player1 = new Pokemon({
-    ...play1,
-    selectors: "player1",
-});
+    getPokemons = async () => {
+        const response = await fetch('https://reactmarathon-api.netlify.app/api/pokemons');
+        const body = await response.json();
+        return body;
 
-const player2 = new Pokemon({
-    ...play2,
-    selectors: "player2",
-});
+    }
 
-const $elImg1 = document.getElementById(`img-${player1.selectors}`);
-$elImg1.src = `${play1.img}`;
+    start = async () => {
+        const pokemons = await this.getPokemons();
 
 
-const $elImg2 = document.getElementById(`img-${player2.selectors}`);
-$elImg2.src = `${play2.img}`;
+        function getRandDataPokemon(in_arr) {
+            const getID = random([0, in_arr.length - 1]);
+            return in_arr.find(item => item.name === in_arr[getID].name);
+        }
 
-const $renderName1 = document.getElementById('name-player1');
-$renderName1 .innerText = `${player1.name}`;
-
-const $renderName2 = document.getElementById('name-player2');
-$renderName2 .innerText = `${player2.name}`;
-
-
-
-const $control = document.querySelector('.control');
-
-player1.attacks.forEach(item => {
-    let $btn = document.createElement('button');
-    $btn.classList.add('button');
-    $btn.innerText = item.name;
-    const btnCount = countBtn(item.maxCount, $btn)
-    $btn.addEventListener('click', () => {
-        // player1.changeHP(random([item.minDamage, item.maxDamage]), function (count) {
-        //     outputLog(generateLog(player1, player2, count));
-        // });
-        player2.changeHP(random([item.minDamage, item.maxDamage]), function (count) {
-            outputLog(generateLog(player2, player1, count));
+        let player1 = new Pokemon({
+            ...getRandDataPokemon(pokemons),
+            selectors: "player1",
         });
-        btnCount();
-    })
-    // Кнопки добавляются из БД первого игрока
-    $control.appendChild($btn);
-});
-player2.attacks.forEach(item => {
-    let $btn = document.createElement('button');
-    $btn.classList.add('button');
-    $btn.innerText = item.name;
-    const btnCount = countBtn(item.maxCount, $btn)
-    $btn.addEventListener('click', () => {
-        player1.changeHP(random([item.minDamage, item.maxDamage]), function (count) {
-            outputLog(generateLog(player1, player2, count));
+
+        let player2 = new Pokemon({
+            ...getRandDataPokemon(pokemons),
+            selectors: "player2",
         });
-        // player2.changeHP(random([item.minDamage, item.maxDamage]), function (count) {
-        //     outputLog(generateLog(player2, player1, count));
+
+        player1.renderCard();
+        player2.renderCard();
+        player1.playerAttacks(player2, player1);
+        player2.playerAttacks(player1, player2);
+
+        // const $renderName1 = document.getElementById('name-player1');
+        //         $renderName1.innerText = `${player1.name}`;
+        // const $renderName2 = document.getElementById('name-player2');
+        //         $renderName2.innerText = `${player2.name}`;
+
+        //
+        //
+        // let $control = document.querySelector('.control');
+        // let $startBtn = document.createElement('button');
+        // $startBtn.classList.add('button');
+        // $startBtn.innerText = 'START GAME!!';
+        // $control.appendChild($startBtn);
+        //
+        // await $startBtn.addEventListener('click', () => {
+        //     $startBtn.remove();
+        //
+        //     function firstPerson() {
+        //         const $elImg1 = document.getElementById(`img-${player1.selectors}`);
+        //         $elImg1.src = `${play1.img}`;
+        //
+        //         const $renderName1 = document.getElementById('name-player1');
+        //         $renderName1.innerText = `${player1.name}`;
+        //
+        //         player1.attacks.forEach(item => {
+        //             let $btn = document.createElement('button');
+        //             $btn.classList.add('button');
+        //             $btn.innerText = item.name;
+        //             const btnCount = countBtn(item.maxCount, $btn)
+        //             $btn.addEventListener('click', () => {
+        //                 let count = random([item.minDamage, item.maxDamage]);
+        //                 // secondPerson.apply(player2.changeHP(firstPerson.apply(count));
+        //                 player2.changeHP(count);
+        //                 outputLog(generateLog(player2, player1, count));
+        //                 btnCount();
+        //             })
+        //
+        //             $control.appendChild($btn);
+        //         });
+        //
+        //     }
+        //
+        //     function secondPerson() {
+        //         //получаем индекс второго персонажа
+        //         const rnd2 = random([0, pokemons.length - 1])
+        //         const play2 = pokemons.find(item => item.name === pokemons[rnd2].name);
+        //
+        //
+        //         const $elImg2 = document.getElementById(`img-${player2.selectors}`);
+        //         $elImg2.src = `${play2.img}`;
+        //
+        //         const $renderName2 = document.getElementById('name-player2');
+        //         $renderName2.innerText = `${player2.name}`;
+
+        // player2.attacks.forEach(item => {
+        //     let $btn = document.createElement('button');
+        //     $btn.classList.add('button');
+        //     $btn.innerText = item.name;
+        //     const btnCount = countBtn(item.maxCount, $btn)
+        //     $btn.addEventListener('click', () => {
+        //         let count = random([item.minDamage, item.maxDamage]);
+        //         player1.changeHP(count);
+        //         outputLog(generateLog(player1, player2, count));
+        //         btnCount();
+        //     })
+        //     $control.appendChild($btn);
         // });
-        btnCount();
-    })
-    $control.appendChild($btn);
+        //
+        //     }
+        //
+        //     firstPerson();
+        //     secondPerson();
+        //
+
+    }
+}
+
+
+let $control = document.querySelector('.control');
+let $startBtn = document.createElement('button');
+$startBtn.classList.add('button');
+$startBtn.innerText = 'START GAME!!';
+$control.appendChild($startBtn);
+$startBtn.addEventListener('click', () => {
+    $startBtn.remove();
+
+
+    const game = new Game();
+    game.start()
+
+
 });
-
-
 
 
 
