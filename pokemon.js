10acab8469b9cb7deb65464b5afa65edf162eb22
Spirit412 <spirit412@gmail.com>
2020-10-13 17:11:13 +0300

    ДЗ-9 Функции перенесены в методы класса

--- pokemon.js
+++ pokemon.js
@@ -1,8 +1,9 @@
-import {generateLog} from "./utils.js";
+import {random, countBtn, generateLog, outputLog} from "./utils.js";
 
 export default class Pokemon {
-    constructor({ name, hp, type, selectors, attacks=[]}) {
+    constructor({ name, hp, type, img, selectors, attacks=[]}) {
         this.name = name;
+        this.img = img;
         this.selectors = selectors;
         this.hp = {
             current: hp,
@@ -24,7 +25,25 @@ export default class Pokemon {
      * @param damage число на которое уменьшается damageHP
      * @param cb
      */
-    changeHP = (damage, cb) => {
+    // changeHP = (damage, cb) => {
+    //     if (damage === undefined)
+    //         damage = 0;
+    //     this.hp.current -= damage;
+    //     if (this.hp.current <= 0) {
+    //         // при 0 жизней у игрока, проходимся по кнопкам и отключаем их
+    //         let btn = document.querySelectorAll('.button');
+    //         btn.forEach($item => $item.disabled = true);
+    //         this.hp.current = 0;
+    //         alert(`${this.name} - Game over!`);
+    //     }
+    //
+    //     this.renderHP();
+    //     cb && cb(damage);
+    // }
+
+    async changeHP (damage) {
+        //Что я не правильно делаю. На старте игры, damage = undefined. Это понятно, т.к. кнопка не нажата.
+        // Победить не смог. Обошел через if
         if (damage === undefined)
             damage = 0;
         this.hp.current -= damage;
@@ -34,10 +53,9 @@ export default class Pokemon {
             btn.forEach($item => $item.disabled = true);
             this.hp.current = 0;
             alert(`${this.name} - Game over!`);
+            this.constructor({...getRandDataPokemon(pokemons), selectors: "player1"})
         }
-
         this.renderHP();
-        cb && cb(damage);
     }
 
     renderHP = () => {
@@ -60,5 +78,37 @@ export default class Pokemon {
             elProgressBar.style.width = `${progressHP}%`
         }
     }
+
+    renderCard = () => {
+        const $renderName1 = document.getElementById(`name-${this.selectors}`);
+        $renderName1.innerText = `${this.name}`;
+
+        const $elImg = document.getElementById(`img-${this.selectors}`);
+                $elImg.src = `${this.img}`;
+
+
+    }
+
+    playerAttacks = (player2, player1) => {
+        this.attacks.forEach(item => {
+            let $control = document.querySelector('.control');
+            let $btn = document.createElement('button');
+            $btn.classList.add('button');
+            $btn.innerText = item.name;
+            const btnCount = countBtn(item.maxCount, $btn)
+
+            $btn.addEventListener('click', () => {
+                let count = random([item.minDamage, item.maxDamage]);
+                player2.changeHP(count);
+                outputLog(generateLog(player2, player1, count));
+                btnCount();
+            })
+
+            $control.appendChild($btn);
+        });
+    }
+
+
+
 
 }
